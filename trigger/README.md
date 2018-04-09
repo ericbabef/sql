e351d4006e4dc397375966456db6e1055311333e
ericbabef <babef.eric@gmail.com>
2018-04-09 09:20:31 +0200

    trigger

---
+++ trigger/README.md
@@ -0,0 +1,3 @@
+Script SQL pour la création d'un trigger basique
+Fonctionnalité : 
+L'ajout d'une nouvelle entité entrainera la mise à jour automatique de plusieurs champs par intéraction avec d'autres layers.
---
+++ trigger/exemple_trigger.sql
@@ -0,0 +1,33 @@
+-- FUNCTION: Création de la fonction - sig.papph_commune_insee_superf()
+
+-- DROP FUNCTION sig.papph_commune_insee_superf();
+
+CREATE FUNCTION sig.papph_commune_insee_superf()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF 
+    COST 100
+AS $BODY$
+BEGIN 
+	NEW.commune := nom_com FROM sig.communes_secteur_papph WHERE sde.st_intersects(NEW.shape, shape);
+	NEW.code_insee := insee_com FROM sig.communes_secteur_papph WHERE sde.st_intersects(NEW.shape, shape);
+	NEW.subdi := subdivision FROM sig.communes_secteur_papph WHERE sde.st_intersects(NEW.shape, shape);
+	NEW.superf_espace := st_area(NEW.shape);	
+	RETURN NEW; 
+	END
+$BODY$;
+
+ALTER FUNCTION sig.papph_commune_insee_superf()
+    OWNER TO sig;
+	
+
+-- Trigger: Ajout du trigger au layer : papph_commune_insee_superf
+
+-- DROP TRIGGER papph_commune_insee_superf ON sig.espacesamenages_cahm;
+
+CREATE TRIGGER papph_commune_insee_superf
+    BEFORE INSERT OR UPDATE 
+    ON sig.espacesamenages_cahm
+    FOR EACH ROW
+    EXECUTE PROCEDURE sig.papph_commune_insee_superf();
